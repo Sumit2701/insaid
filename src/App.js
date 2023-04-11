301c08b96d1c0e81442abbec606f9526501df996
Sumit2701 <sumitahire2701@gmail.com>
2023-04-11 21:42:55 +0530

    Final Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import { AboutUs } from './AboutUs';
 import './App.css';
-
+import {Home} from './Home';
+import { Navbar } from './components/navbar';
+import {Routes, BrowserRouter as Router,Route} from 'react-router-dom'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Router>
+      <Navbar/>
+      <Routes>
+      <Route path="" element={<Home />} />   
+      <Route path="/AboutUs" element={<AboutUs />} />         
+      </Routes>
+     </Router>
+
     </div>
   );
 }
